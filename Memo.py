1f951dbd9eb8ff316172fc83fd9b3e203b073b46
Sijey <80583308+sijey-praveen@users.noreply.github.com>
2021-05-08 00:06:42 -0700

    Update Memo.py

--- Memo.py
+++ Memo.py
@@ -1,25 +1,30 @@
 import discord
 from discord.ext import commands
 import requests
+from bs4 import BeautifulSoup
 
 client = commands.Bot(command_prefix="!")
 
 @client.command()
 async def hello(ctx):
-    command_list = "!\n!download\n!play\n!spotify\n!soundcloud\n!ytm\n!yt"
-    await ctx.send(command_list)
+    command_list = "\n!ytdownload\n!spotify\n!soundcloud\n!ytmusic\n!yt\n!google\n!medium\n!github\n!weather\n!gmaps\n!maps\n!ddg\n!wiki"
+    await ctx.send("Hello" + command_list)
 
 @client.command()
 async def hi(ctx):
-    command_list = "!\n!download\n!play\n!spotify\n!soundcloud\n!ytm\n!yt"
-    await ctx.send(command_list)
+    await ctx.send("Hi")
 
 @client.command()
-async def play(ctx, *, message):
-    url = "https://pywhatkit.herokuapp.com/playonyt?topic=" + message
-    GET_content = requests.get(url)
-    Play_Content = str(GET_content.text)
-    await ctx.send(Play_Content)
+async def weather(ctx, *, message):
+    url = "https://www.google.com/search?q=weather " + message
+    html = requests.get(url).content
+    soup = BeautifulSoup(html, 'html.parser')
+    temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
+    str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
+    data = str.split('\n')
+    sky = data[1]
+    result = "Temperature was currently " + temp.replace("C", "celcius")
+    await ctx.send(result)
 
 @client.command()
 async def yt(ctx, *, message):
@@ -29,7 +34,38 @@ async def yt(ctx, *, message):
     await ctx.send(Play_Content)
 
 @client.command()
-async def download(ctx, *, message):
+async def medium(ctx, *, message):
+    url = "https://medium.com/search?q=" + message.replace(" ", "%20")
+    await ctx.send(url)
+
+@client.command()
+async def google(ctx, *, message):
+    url = "https://www.google.com/search?q=" + message.replace(" ", "+")
+    await ctx.send(url)
+
+@client.command()
+async def wiki(ctx, *, message):
+    url = "https://en.wikipedia.org/wiki/" + message.replace(" ", "_")
+    await ctx.send(url)
+
+@client.command()
+async def ddg(ctx, *, message):
+    url = "https://duckduckgo.com/?t=ffab&q=" + message.replace(" ", "+")
+    await ctx.send(url)
+
+@client.command()
+async def about(ctx):
+    about = "Hello, i'm Memo.\nI Live in Discord.\nI provide url for User Queries.\nFor more information \nvisit : https://github.com/sijey-praveen/Memo"
+    await ctx.send(about)
+
+@client.command()
+async def github(ctx, *, message):
+    if "trending" in message:
+        trending = "https://github.com/trending"
+        await ctx.send(trending)
+
+@client.command()
+async def ytdownload(ctx, *, message):
     url = "https://pywhatkit.herokuapp.com/playonyt?topic=" + message
     GET_content = requests.get(url)
     dl = GET_content.text.replace("youtube", "youtubepp")
@@ -47,18 +83,28 @@ async def soundcloud(ctx, *, message):
 
 
 @client.command()
-async def ytm(ctx, *, message):
+async def ytmusic(ctx, *, message):
     url = "https://music.youtube.com/search?q=" + message.replace(" ", "+")
     await ctx.send(url)
 
 @client.command()
+async def googlemaps(ctx, *, message):
+    url = "https://www.google.com/maps/search/" + message.replace(" ", "+")
+    await ctx.send(url)
+
+@client.command()
+async def maps(ctx, *, message):
+    url = "https://duckduckgo.com/?t=ffab&q=" + message.replace(" ", "+") + "&atb=v271-2&ia=web&iaxm=maps"
+    await ctx.send(url)
+
+@client.command()
 async def memo(ctx):
-    command_list = "!hi\n!hello\n!dl\n!download\n!play"
+    command_list = "\n!ytdownload\n!spotify\n!soundcloud\n!ytmusic\n!yt\n!google\n!medium\n!github\n!weather\n!gmaps\n!maps\n!ddg\n!wiki"
     await ctx.send(command_list)
 
 @client.command()
 async def commands(ctx):
-    command_list = "!\n!download\n!play\n!spotify\n!soundcloud\n!ytm\n!yt"
+    command_list = "\n!ytdownload\n!spotify\n!soundcloud\n!ytmusic\n!yt\n!google\n!medium\n!github\n!weather\n!gmaps\n!maps\n!ddg\n!wiki"
     await ctx.send(command_list)
-
+    
 client.run("Your_Token")
