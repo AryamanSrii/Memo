24c8067dce241ac03d3e4a03df56149a6178a98b
Sijey <80583308+sijey-praveen@users.noreply.github.com>
2021-05-07 21:26:22 -0700

    Update Memo.py

--- Memo.py
+++ Memo.py
@@ -6,11 +6,13 @@ client = commands.Bot(command_prefix="!")
 
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hello")
+    command_list = "!\n!download\n!play\n!spotify\n!soundcloud\n!ytm\n!yt"
+    await ctx.send(command_list)
 
 @client.command()
 async def hi(ctx):
-    await ctx.send("Hi")
+    command_list = "!\n!download\n!play\n!spotify\n!soundcloud\n!ytm\n!yt"
+    await ctx.send(command_list)
 
 @client.command()
 async def play(ctx, *, message):
@@ -21,8 +23,10 @@ async def play(ctx, *, message):
 
 @client.command()
 async def yt(ctx, *, message):
-    url = "https://www.youtube.com/results?search_query=" + message
-    await ctx.send(url)
+    url = "https://pywhatkit.herokuapp.com/playonyt?topic=" + message
+    GET_content = requests.get(url)
+    Play_Content = str(GET_content.text)
+    await ctx.send(Play_Content)
 
 @client.command()
 async def download(ctx, *, message):
@@ -32,11 +36,20 @@ async def download(ctx, *, message):
     await ctx.send(dl)
 
 @client.command()
-async def dl(ctx, *, message):
-    url = "https://pywhatkit.herokuapp.com/playonyt?topic=" + message
-    GET_content = requests.get(url)
-    dl = GET_content.text.replace("youtube", "youtubepp")
-    await ctx.send(dl)
+async def spotify(ctx, *, message):
+    url = "https://open.spotify.com/search/" + message.replace(" ", "%20")
+    await ctx.send(url)
+
+@client.command()
+async def soundcloud(ctx, *, message):
+    url = "https://soundcloud.com/search?q=" + message.replace(" ", "%20")
+    await ctx.send(url)
+
+
+@client.command()
+async def ytm(ctx, *, message):
+    url = "https://music.youtube.com/search?q=" + message.replace(" ", "+")
+    await ctx.send(url)
 
 @client.command()
 async def memo(ctx):
@@ -45,7 +58,7 @@ async def memo(ctx):
 
 @client.command()
 async def commands(ctx):
-    command_list = "!hi\n!hello\n!dl\n!download\n!play"
+    command_list = "!\n!download\n!play\n!spotify\n!soundcloud\n!ytm\n!yt"
     await ctx.send(command_list)
 
 client.run("Your_Token")
