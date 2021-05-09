989b510a14551ccc715c9627660f2138bc749053
Sijey <80583308+sijey-praveen@users.noreply.github.com>
2021-05-09 03:05:33 -0700

    Update Memo.py

--- Memo.py
+++ Memo.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import requests
 from bs4 import BeautifulSoup
 import wikipedia
-
+from covid_india import states
 
 client = commands.Bot(command_prefix="!")
 
@@ -18,23 +18,22 @@ async def hi(ctx):
 
 @client.command()
 async def memo(ctx):
-    wmcd = """Memo, A Discord Bot.
-What Memo Can Do?
-    - Provide Results From Wikipedia.
-    - Helps You to navigate <Your_Location> to <Destination_Location> using Google Maps.
-    - Provide the URL For Your Query from YouTube.
-    - Provide the weather Forecast From your Country/State/City.\n
-# Commands:
-!maps Your_Location > Destination_Location
-!memo This Refers To About Memo.
-!wiki stevejobs
-!yt play faded
-!weather Your_Location
-!hi refers General (Don't be spam).
-!hello refers General (Don't be spam).
-For more Command Ideas Create a issues and mention there, Visit : https://github.com/sijey-praveen/Memo/issues 
-"""
-    await ctx.send(wmcd)
+    embed = discord.Embed(title='Memo, a Discord Bot',
+                      url='https://github.com/sijey-praveen/Memo/issues', color=000000)
+
+    embed.set_thumbnail(url='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fherobot.app%2Fwp-content%2Fuploads%2F2020%2F10%2FAI-bot-1.jpg&f=1&nofb=1')
+
+    embed.set_author(name="Memo Bot", url="https://github.com/sijey-praveen/Memo/issues", icon_url="https://cdn.discordapp.com/attachments/840203029793996811/840868393396600842/VisualElements_512.png")
+
+    embed.add_field(name="What Memo can do?", value="> Provide Results From Wikipedia.\n> Helps You to navigate <Your_Location> to <Destination_Location> using Google Maps.\n> Provide the URL For Your Query from YouTube.\n> Provide the weather Forecast From your Country/State/City.", inline=False)
+
+    embed.add_field(name="Commands", value="> `!maps <your_location> > <destination_location>`\n> `!memo` This Refers To About Memo.\n> `!wiki <what_to_search>`\n> `!yt play <what_to_play>`\n> `!weather <your_location>`\n> `!hi` refers General (Don't be spam).\n> `!hello` refers General (Don't be spam).", inline=False)
+
+    embed.add_field(name='For more commands ideas', value='> Create a issue and mention there\n> Visit the [GitHub page](https://github.com/sijey-praveen/Memo/issues)', inline=False)
+
+    embed.set_footer(text="Information requested by: {}".format(ctx.author.display_name))
+
+    await ctx.channel.send(embed=embed)
 
 # Main Commands
 @client.command()
@@ -45,8 +44,13 @@ async def weather(ctx, *, message):
     temp = soup.find('div', attrs={'class': 'BNeawe iBp4i AP7Wnd'}).text
     str = soup.find('div', attrs={'class': 'BNeawe tAd8D AP7Wnd'}).text
     data = str.split('\n')
+    time = data[0]
     sky = data[1]
-    result = "In " + message + " the Temperature was currently " + temp.replace("C", "celcius")
+    listdiv = soup.findAll('div', attrs={'class': 'BNeawe s3v9rd AP7Wnd'})
+    strd = listdiv[5].text
+    pos = strd.find('Wind')
+    other_data = strd[pos:]
+    result = "In " + message + " the Temperature was currently " + temp.replace("C", "celcius") + ", The Sky was now " + sky + " " + other_data
     await ctx.send(result)
 
 @client.command()
@@ -67,7 +71,21 @@ async def wiki(ctx, *, message):
 
 @client.command()
 async def maps(ctx, *, message):
-    url = "https://www.google.com/maps/dir/" + message.replace(">", "/")
+    url = "https://www.google.com/maps/dir/" + message.replace(">", "/").replace(" ", "")
+    await ctx.send(url)
+
+@client.command()
+async def covid(ctx, *, message):
+    if "india" in message.lower():
+        result = states.getdata()
+        await ctx.send(result)
+    else:
+        result = states.getdata(message)
+        await ctx.send(result)
+
+@client.command()
+async def coding(ctx, *, message):
+    url = "https://stackoverflow.com/search?q=" + message.replace(" ", "+")
     await ctx.send(url)
     
 client.run("Your_Token")
