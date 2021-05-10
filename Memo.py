6299d9651b21fea5b4abb9444a3130e8a73078dc
Sijey <80583308+sijey-praveen@users.noreply.github.com>
2021-05-09 22:13:53 -0700

    Update Memo.py

--- Memo.py
+++ Memo.py
@@ -4,6 +4,7 @@ import requests
 from bs4 import BeautifulSoup
 import wikipedia
 from covid_india import states
+import pyjokes
 
 client = commands.Bot(command_prefix="!")
 
@@ -23,11 +24,11 @@ async def memo(ctx):
 
     embed.set_thumbnail(url='https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fherobot.app%2Fwp-content%2Fuploads%2F2020%2F10%2FAI-bot-1.jpg&f=1&nofb=1')
 
-    embed.set_author(name="Memo Bot", url="https://github.com/sijey-praveen/Memo/issues", icon_url="https://cdn.discordapp.com/attachments/840203029793996811/840868393396600842/VisualElements_512.png")
+    embed.set_author(name="Memo Bot", url="https://github.com/sijey-praveen/Memo/", icon_url="https://cdn.discordapp.com/attachments/840203029793996811/840868393396600842/VisualElements_512.png")
 
-    embed.add_field(name="What Memo can do?", value="> Provide Results From Wikipedia.\n> Helps You to navigate <Your_Location> to <Destination_Location> using Google Maps.\n> Provide the URL For Your Query from YouTube.\n> Provide the weather Forecast From your Country/State/City.", inline=False)
+    embed.add_field(name="What Memo can do?", value="> Provide Results From Wikipedia.\n> Helps You to navigate <Your_Location> to <Destination_Location> using Google Maps.\n> Provide the URL For Your Query from YouTube.\n> Provide the weather Forecast From your Country/State/City.\n> Tell jokes When You're Bored.", inline=False)
 
-    embed.add_field(name="Commands", value="> `!maps <your_location> > <destination_location>`\n> `!memo` This Refers To About Memo.\n> `!wiki <what_to_search>`\n> `!yt play <what_to_play>`\n> `!weather <your_location>`\n> `!hi` refers General (Don't be spam).\n> `!hello` refers General (Don't be spam).", inline=False)
+    embed.add_field(name="Commands", value="> `!maps <your_location> > <destination_location>`\n> `!memo` This Refers To About Memo.\n> `!wiki <what_to_search>`\n> `!yt play <what_to_play>`\n> `!weather <your_location>`\n> `!hi` refers General (Don't be spam).\n> `!hello` refers General (Don't be spam).\n> `!joke` refers Jokes.", inline=False)
 
     embed.add_field(name='For more commands ideas', value='> Create a issue and mention there\n> Visit the [GitHub page](https://github.com/sijey-praveen/Memo/issues)', inline=False)
 
@@ -51,7 +52,8 @@ async def weather(ctx, *, message):
     pos = strd.find('Wind')
     other_data = strd[pos:]
     result = "In " + message + " the Temperature was currently " + temp.replace("C", "celcius") + ", The Sky was now " + sky + " " + other_data
-    await ctx.send(result)
+    em=discord.Embed(title=result,color=discord.Color.red())
+    await ctx.send(embed=em)
 
 @client.command()
 async def yt(ctx, *, message):
@@ -64,7 +66,8 @@ async def yt(ctx, *, message):
 async def wiki(ctx, *, message):
     try:
         result_from_wiki = wikipedia.summary(str(message), sentences=5)
-        await ctx.send(result_from_wiki)
+        em=discord.Embed(title=message,color=discord.Color.red(), description=result_from_wiki)
+        await ctx.send(embed=em)
     except Exception:
         url = "https://en.wikipedia.org/wiki/" + message.replace(" ", "_")
         await ctx.send(url)
@@ -73,6 +76,13 @@ async def wiki(ctx, *, message):
 async def maps(ctx, *, message):
     url = "https://www.google.com/maps/dir/" + message.replace(">", "/").replace(" ", "")
     await ctx.send(url)
+
+
+@client.command()
+async def joke(ctx):
+    random_joke = pyjokes.get_joke()
+    em=discord.Embed(title=random_joke,color=discord.Color.red())
+    await ctx.send(embed=em)
 
 @client.command()
 async def covid(ctx, *, message):
